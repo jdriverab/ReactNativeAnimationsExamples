6c90e5c5d1f2f172d100645ca15f8702aaaeccb9
jdriverab <jdriverab@hotmail.com>
2022-12-30 14:15:36 -0300

    se agregan animaciones para track de gestos

--- src/screens/EventsAnimationScreen.tsx
+++ src/screens/EventsAnimationScreen.tsx
@@ -1,11 +1,74 @@
 import React, {useState} from 'react'
-import { Animated, View, Text, ScrollView } from 'react-native';
+import { Animated, View, Text, ScrollView, PanResponder } from 'react-native';
 import { GlobalStyle } from './RegularAnimationsScreen';
+
+const ComponentEventPan = () => {
+    const animationPan = new Animated.ValueXY({x:0, y:0})
+
+    const animatedStyle = {
+        transform: animationPan.getTranslateTransform() 
+    }
+
+    const EventComponent = () => {
+        let _x = 0
+        let _y = 0
+
+        animationPan.addListener((value)=>{
+            _x = value.x,
+            _y = value.y
+        })
+        
+        return PanResponder.create({
+                onStartShouldSetPanResponder:()=> true,
+                onMoveShouldSetPanResponder:()=> true,
+                onPanResponderGrant: ()=>{
+                    animationPan.extractOffset()
+                    /*
+                    animationPan.setOffset({
+                        x: _x,
+                        y: _y
+                    });
+                    animationPan.setValue({
+                        x:0,
+                        y:0
+                    })
+                    */
+                },
+
+                onPanResponderMove: Animated.event([
+                    // null porque no se espera evento, el segundo objeto es por los gestos
+                    null, {
+                    dx: animationPan.x,
+                    dy: animationPan.y
+                }],
+                {useNativeDriver:false}),
+
+                onPanResponderRelease: (e, {vx, vy})=>{
+                    Animated.decay(animationPan,{
+                        velocity:{
+                            x:vx,
+                            y:vy 
+                        },
+                        deceleration:0.997,
+                        useNativeDriver:false
+                    }).start()
+                },
+        })
+    }
+
+    return(
+        <Animated.View 
+        style={[{height:80, width:80, position:'absolute', top:'40%', right:'45%', backgroundColor:'red'}, animatedStyle]}
+        {... EventComponent().panHandlers}
+        />
+
+    )
+
+}
 
 
     const ComponentEventColor = () => {
 
-        //const [animationColor, setAnimationColor] = useState(new Animated.Value(0))
 
         const animationColors = new Animated.Value(0)
     
@@ -27,7 +90,7 @@ import { GlobalStyle } from './RegularAnimationsScreen';
                     Animated.event([
                         { nativeEvent: {
                             contentOffset: {
-                            x: animationColors
+                            y: animationColors
                             }
                         }}
                         ],
@@ -35,7 +98,9 @@ import { GlobalStyle } from './RegularAnimationsScreen';
                     )
                 }>
 
-                    <Animated.View style={[{height:1500}, animatedStyle]}/>
+                    <Animated.View style={[{height:2000}, animatedStyle]}/>
+
+
                         
 
                 </ScrollView>
@@ -45,9 +110,9 @@ import { GlobalStyle } from './RegularAnimationsScreen';
     
     const EventsAnimationScreen = () => {
       return (
-        <View style={{ flex: 1}}>
-            <ComponentEventColor/>
-    
+        <View style={{ flex: 1, /*justifyContent:'center', alignItems:'center'*/}}>
+            {<ComponentEventColor/>}
+            <ComponentEventPan/>
         </View>
       )
     }
